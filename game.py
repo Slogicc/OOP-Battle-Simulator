0afd877749745444954567e54d2392d6ee7da5b8
Joshua <136170772+Slogicc@users.noreply.github.com>
2026-09-14 10:51:02 -0500

    Built Hero Class

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "bottomless hole"
 
@@ -12,10 +12,19 @@ def main():
 
     goblin = Goblin("Schlork")
     goblin2 = Goblin("Scribble")
+    hero = Hero("1BirdInTheHandIsWorth2InTheBush", "Janitor")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
-    print("But no hero has answered the call... yet.")
+    print(f"{hero.name} enters te arena with {hero.health} health. He is a {hero.battle_class}.")
+    damageDealt = hero.attack()
+    print(f"{hero.name} attacks!")
+    goblin.take_damage(damageDealt)
+    if goblin.is_alive():
+        damageDealt = goblin.attack()
+        print(f"{goblin.name} attacks!")
+        hero.take_damage(damageDealt)
+
 
 
 if __name__ == "__main__":
--- hero.py
+++ hero.py
@@ -1,4 +1,25 @@
-class Hero:
-    """The hero blueprint will be implemented later in the project."""
+import random as r
 
-    pass
+class Hero:
+    def __init__(self, name, battle_class):
+        self.name = name
+        self.health = r.randint(100,150)
+        self.attack_power = r.randint(10,25)
+        self.battle_class = battle_class
+
+    def attack(self):
+        return r.randint(1, self.attack_power)
+
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+            print(f"{self.name} takes {damage} damage. {self.name} is dead.")
+        else:
+            print(f"{self.name} takes {damage} damage. {self.health} health remaining.")
+
+    def is_alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
